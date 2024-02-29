abbce13c6f14efd1392bafec83b9fc5b9b7b565a
Yodgorbekova <marjonayodgorbekova505@gmail.com>
2024-02-29 13:48:20 +0500

    fix

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+                          //UYGA VAZIFA//
+                    //HTML Css and Java-script//
 let big__box = document.querySelector(".big__box")
-let box = document.querySelector(".box")
 let center = document.querySelector(".center")
 let around = document.querySelector(".around")
 let between = document.querySelector(".between")
 let end = document.querySelector(".end")
+let start = document.querySelector(".start")
 
+start.onclick = () => {
+    big__box.style.justifyContent = "start"
+}
 center.onclick = () => {
     big__box.style.justifyContent = "center"
 }
